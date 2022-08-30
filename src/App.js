2144dd20bb21a9357f5ce1e44a5177433563fd81
laxmanprometteur <lnemane7@gmail.com>
2022-08-30 17:00:50 +0530

    basic react project setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      Hello Lakhan you can do it in two days
+      <button className="btn-primary">Hello lakhan</button>
+      <a href="#">Hello</a>
+    </div>
+  );
+}
+
+export default App;
